764553eb9c2af6ccef1e339f6f52fe33bbaf75ad
daniilkukalo <56275096+daniilkukalo@users.noreply.github.com>
2021-03-26 18:58:13 +0200

    update

--- js.js
+++ js.js
@@ -1,27 +1,33 @@
 $(document).ready(function(){
 $(".maslo").knob({
-	'angleArc':90 ,
-	'angleOffset':-140,
+	'angleArc':360 ,
 	'min':0,
 	'max':70
 });
 $(".tacho").knob({
-	'angleArc': 220 ,
-	'angleOffset':-110,
+	'angleArc': 360 ,
 	'min':0,
 	'max':8000
 });
 $(".speed").knob({
-	'angleArc':220 ,
-	'angleOffset':-110,
+	'angleArc':360 ,
 	'min':0,
 	'max':320
 });
-$(".benz").knob({
-	'angleArc':90 ,
-	'angleOffset':50,
-	'min':0,
-	'max':80,
-	'rotation':'anticlockwise'
 });
-});
+
+
+
+function clock(){
+	var $s = $(".second"),
+	$m = $(".minute"),
+	$h = $(".hour");
+	d = new new Date(),
+	s = d.getSeconds(),
+	m = d.getMinutes(),
+	h = d.getHours();
+	$s.val(s).trigger("change");
+	$m.val(m).trigger("change");
+	$h.val(h).trigger("change");
+	setTimeout("clock()", 1000)
+}
