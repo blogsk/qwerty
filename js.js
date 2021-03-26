66da8b4066631905c9aed68ed1b504cba9e70b53
daniilkukalo <56275096+daniilkukalo@users.noreply.github.com>
2021-03-26 18:25:29 +0200

    update

--- js.js
+++ js.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-$(".dial").knob();
+$(".maslo").knob({
+	'angleArc':90 ,
+	'angleOffset':-140,
+	'min':0,
+	'max':70
+});
+$(".tacho").knob({
+	'angleArc': 220 ,
+	'angleOffset':-110,
+	'min':0,
+	'max':8000
+});
+$(".speed").knob({
+	'angleArc':220 ,
+	'angleOffset':-110,
+	'min':0,
+	'max':320
+});
+$(".benz").knob({
+	'angleArc':90 ,
+	'angleOffset':50,
+	'min':0,
+	'max':80,
+	'rotation':'anticlockwise'
+});
 });
